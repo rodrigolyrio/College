895512b089144ecd76ad439b4b1b9bf152662450
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-08-12 09:21:00 -0300

    Create Exercicio1.py

---
+++ Programação I/Exercicio1.py
@@ -0,0 +1,9 @@
+def main():
+    a = int(input("digite um valor: "))
+    b = int(input("digite outro valor: "))
+    soma = a + b
+    print(soma)
+    return 0
+    
+if __name__ == "__main__":
+    main()
