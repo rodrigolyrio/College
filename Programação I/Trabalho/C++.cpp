151beaad78b22207f05466fa29398c4a471dfa09
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-08-13 12:39:55 -0300

    Create C++.cpp

---
+++ Programação I/Trabalho/C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string nome;
+
+    std::cout << "Digite o nome: ";
+    std::cin >> nome;
+
+    std::cout << "Olá, " << nome << std::endl;
+
+    return 0;
+}
