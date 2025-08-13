81b4ece1dd144fb95c1612d3c13da2dee204b437
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-08-13 12:40:34 -0300

    Create C#.cs

---
+++ Programação I/Trabalho/C#.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {nome}");
+    }
+}
