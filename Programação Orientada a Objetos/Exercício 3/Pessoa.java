b0e8677275d543b9e04530ed11844ad2abcee14d
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-08-20 19:54:07 -0300

    Create Pessoa.java

---
+++ Programação Orientada a Objetos/Exercício 3/Pessoa.java
@@ -0,0 +1,25 @@
+package banco;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+    Data nascimento;
+    char sexo;
+
+    Pessoa(String n, String c, Data nasc, char s) {
+        this.nome = n;
+        this.cpf = c;
+        this.nascimento = nasc;
+        this.sexo = s;
+    }
+
+    int idade(Data hoje) {
+        int idade = hoje.ano - this.nascimento.ano;
+
+        if (hoje.mes < this.nascimento.mes || (hoje.mes == this.nascimento.mes && hoje.dia < this.nascimento.dia)) {
+            idade -= 1;
+        }
+
+        return idade;
+    }
+}
