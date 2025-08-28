e0c56dc145a4a6bf5e0ca9d49a38fc9166d899dc
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-08-28 08:10:39 -0300

    Create Exercicio15.py

---
+++ Programação I/Exercicio15.py
@@ -0,0 +1,11 @@
+def main():
+    n1 = float(input('n1: '))
+    n2 = float(input('n2: '))
+    n3 = float(input('n3: '))
+    n4 = float(input('n4: '))
+    media = (n1+n2+n3+n4)/4
+    print(media)
+    
+    return 0
+if __name__ == "__main__":
+    main()
