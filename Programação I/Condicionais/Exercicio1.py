c27b509118fb37b5100468080154b858e02b4361
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-01 12:58:32 -0300

    Create Exercicio1.py

---
+++ Programação I/Condicionais/Exercicio1.py
@@ -0,0 +1,23 @@
+def main():
+  a = 10
+  b = 20
+  c = 30
+  d = 40
+  if (c > a):
+  if (d > c):
+  print("s")
+  if (c > b):
+  print("a")
+  else:
+  print("o")
+  if (a < b):
+  print ("c")
+  else:
+  print("r")
+  print("p")
+  if (b < c):
+  print("o")
+  return 0
+
+if __name__ == "__main__":
+  main()
