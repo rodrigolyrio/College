42e77c736ad44fa5a5e7b4fbffc102734c17ba62
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-02 17:44:06 -0300

    Create Exercicio4.py

---
+++ Programação I/Condicionais/Exercicio4.py
@@ -0,0 +1,10 @@
+def main():
+    a = int(input("a: "))
+    b = int(input("b: "))
+    if (a > b):
+        print(a+b)
+    else:
+        print(a*b)
+    return 0
+if __name__ == "__main__":
+    main()
