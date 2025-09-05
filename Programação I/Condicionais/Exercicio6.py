ba287181ce030c5db348c2e520c71c0d9edd996d
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-05 15:59:07 -0300

    Create Exercicio6.py

---
+++ Programação I/Condicionais/Exercicio6.py
@@ -0,0 +1,12 @@
+def main():
+    n1 = float(input("n1: "))
+    n2 = float(input("n2: "))
+    n3 = float(input("n3: "))
+    n4 = float(input("n4: "))
+    media = (n1+n2+n3+n4)/4
+    print(media)
+    if (media >= 60):
+        print("APROVADO")
+    return 0
+if __name__ == "__main__":
+    main()
