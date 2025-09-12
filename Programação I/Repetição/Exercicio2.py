e10e9d2b9a57e0025f66632aeca85ff30b5b0c63
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-12 11:06:40 -0300

    Create Exercicio2.py

---
+++ Programação I/Repetição/Exercicio2.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(0)
+    i = int(0)
+    i = 1
+    n = int(input("valor: "))
+    while (i <= 10):
+        print(f'{i} x {n} = {i*n}')
+        i = i + 1
+    
+    
+    return 0
+if __name__ == "__main__":
+    main()
